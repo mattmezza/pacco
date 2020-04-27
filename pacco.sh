f1b92a443a2beba4d83493cabac3d6773f455c49
Matteo Merola <mattmezza@gmail.com>
2020-04-27 19:17:12 +0200

    Initial commit

---
+++ pacco.sh
@@ -0,0 +1,73 @@
+pacco() {
+    local MANIFEST=${PACCO_FILE:-"pacco.txt"}
+    local DIR=${PACCO_DIR:-"pacchi"}
+    local VERSION="1.0.0"
+    case $1 in
+        -h|--help)
+            cat <<- EOF
+Usage:
+    $0 [OPT] CMD ARGS
+
+CMD:
+     l|list
+     i|install
+     u|uninstall
+     s|source
+    ia|inatall-all
+    ua|uninstall-all
+    sa|source-all
+
+OPT:
+    -h|--help
+    -v|--version
+
+Examples:
+    $ $0 list                # lists all pkgs
+    $ $0 i name git-url tag  # installs pkg 'name' @tag via 'url'
+    $ $0 u name              # uninstalls pkg 'name'
+    $ $0 s name              # sources pkg 'name'
+    $ $0 ia                  # installs all pkgs
+    $ $0 ua                  # uninstalls all pkgs
+    $ $0 sa                  # sources all the pkgs
+    $ $0 -v                  # prints the pacco version
+    $ $0 -h                  # prints this message
+EOF
+            ;;
+        -v|--version)
+            echo $VERSION
+            ;;
+        l|list)
+            cat $MANIFEST | sort
+            ;;
+        u|uninstall)
+            local WHAT=$2
+            [ -d "$DIR/$WHAT" ] && rm -rf "$DIR/$WHAT" && echo "Uninstalled '$WHAT'"
+            ;;
+        s|source)
+            local WHAT=$2
+            source "$DIR/$WHAT/pacco"
+            ;;
+        i|install)
+            local WHAT=$2
+            local WHERE=$3
+            local WHICH=$4
+            echo "Installing '$WHAT:$WHICH'..."
+            mkdir -p "$DIR/$WHAT"
+            git clone -q --branch $WHICH $WHERE "$DIR/$WHAT" > /dev/null 2> /dev/null
+            echo "Installed '$WHAT:$WHICH'"
+            ;;
+        ia|install-all)
+            source <(cat $MANIFEST | while read in; do echo "$0 i $in"; done)
+            ;;
+        ua|uninstall-all)
+            source <(cat $MANIFEST | while read in; do echo "$0 u $in"; done)
+            ;;
+        sa|source-all)
+            source <(cat $MANIFEST | while read in; do echo "$0 s $in"; done)
+            ;;
+        *)
+            echo $($0 -h)
+            return 1
+            ;;
+    esac
+}
---
+++ readme.md
@@ -0,0 +1,65 @@
+pacco
+===
+
+`pacco` is a utility to allow you to quickly install or uninstall plugins with custom alias or functions.
+
+![](j.gif)
+It works with a simple plain text file `pacco.txt` that lists the wanted packages in this format:
+```
+jump  git@github.com:mattmezza/jump.git   1.0.0
+sp    git@github.com:mattmezza/sp.git     0.1.0
+spot  https://github.com/rauchg/spot.git  0.2.0
+
+```
+You can override the name of the used file by setting an env variable `PACCO_FILE="your-pacco.txt"`.
+
+When run, `pacco` will install the packages in a default directory `pacchi` (you can override this location via the env var `PACCO_DIR`).
+
+
+Installation
+===
+
+`source <(curl -s https://raw.githubusercontent.com/mattmezza/pacco/1.0.0/pacco.sh) && pacco i pacco https://github.com/mattmezza/pacco.git 1.0.0`
+
+Installing `pacco` is very easy: just source the `pacco.sh` in this repo. Here's how I do it in my [dotfiles](https://github.com/mattmezza/dotfiles).
+
+Usage
+===
+
+What follows is the output of `pacco -h`, accessible at any time.
+
+```
+Usage:
+    pacco [OPT] CMD ARGS
+
+CMD:
+     l|list
+     i|install
+     u|uninstall
+     s|source
+    ia|inatall-all
+    ua|uninstall-all
+    sa|source-all
+
+OPT:
+    -h|--help
+    -v|--version
+
+Examples:
+    $ pacco list                # lists all pkgs
+    $ pacco i name git-url tag  # installs pkg 'name' @tag via 'url'
+    $ pacco u name              # uninstalls pkg 'name'
+    $ pacco s name              # sources pkg 'name'
+    $ pacco ia                  # installs all pkgs
+    $ pacco ua                  # uninstalls all pkgs
+    $ pacco sa                  # sources all the pkgs
+    $ pacco -v                  # prints the pacco version
+    $ pacco -h                  # prints this message
+
+```
+
+Development
+===
+
+Developing `pacco` is quite easy. Clone the repo and start editing the `pacco.sh` file. You can always source your verions and test it in your shell.
+
